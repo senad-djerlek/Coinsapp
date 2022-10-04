dc7ff37ba649eb7cb519132d28906a0a01cad960
denismahmutovic <djdeny95@gmail.com>
2022-10-04 17:01:02 +0200

    coin page with id

--- src/pages/Coin/Coin.js
+++ src/pages/Coin/Coin.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 
 function Coin() {
-  const [data, setData] = useState();
+  const [coin, setCoin] = useState();
   const { uuid } = useParams();
 
   const options = {
@@ -18,7 +18,7 @@ function Coin() {
   const getData = () => {
     axios.request(options).then(function (response) {
       console.log(response.data.data.coin);
-      setData(response.data.data.coin);
+      setCoin(response.data.data.coin);
     });
   };
 
@@ -28,7 +28,167 @@ function Coin() {
 
   return (
     <div>
-      <div>{data?.name}</div>
+      <div className="mt-9" />
+      <div className="flex ml-2 mt-3">
+        <img
+          src="https://flowbite.com/docs/images/logo.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Coins Logo"
+        />
+        <h1>crypto List</h1>
+      </div>
+      <hr />
+      <div className="flex mt-2  items-center p-4 ml-2 justify-around">
+        <img src={coin?.iconUrl} width={30} />
+        <p className="mr-13">{coin?.name}</p>
+        <p>{coin?.symbol}</p>
+        <p className="border-x-4 border-grey-300"> # {coin?.rank}</p>
+        <p>
+          $ {coin?.price}{" "}
+          <span className="border-2 border-grey-200 font-serif">Live</span>
+        </p>
+      </div>
+      <hr />
+      <div className="flex items-center justify-around mt-10 ">
+        <h3>Price chart </h3>
+        <p>24h{coin?.change} %</p>
+        <p>High {coin?.supply?.max} </p>
+      </div>
+      <hr />
+      <div className="mt-4/2 mb-12">
+        <img src=" https://i.im.ge/2022/10/03/1VTQ2y.Screenshot-2022-10-03-at-15-54-47.png" />
+      </div>
+      <div className="flex">
+        <div>
+          <div className="ml-2 text-lg">Value statistics</div>
+          <div>
+            <p>
+              An overview showing the statistics of Bitcoin, such as the base
+              and quote currency, the rank, and trading volume.
+            </p>
+          </div>
+          <div className="flex items-center justify-around">
+            <svg
+              class="h-10 w-5 text-blue-500"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{" "}
+              <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
+            </svg>
+            <h1>Price to EUR</h1>
+            <h3>{coin?.price}</h3>
+          </div>
+          <div className="flex items-center justify-around">
+            <svg
+              class="h-10 w-5 text-blue-500"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{" "}
+              <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
+            </svg>
+            <h1>Price to BTC</h1>
+            <h3>{coin?.price}</h3>
+          </div>
+          <div className="flex items-center justify-around">
+            <svg
+              class="h-10 w-5 text-blue-500"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{" "}
+              <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
+            </svg>
+            <h1>Rank</h1>
+            <h3>{coin?.price}</h3>
+          </div>
+          <div className="flex items-center justify-around">
+            <svg
+              class="h-10 w-5 text-blue-500"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{" "}
+              <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
+            </svg>
+            <h1>24h volume</h1>
+            <h3>{coin?.price}</h3>
+          </div>
+          <div className="flex items-center justify-around">
+            <svg
+              class="h-10 w-5 text-blue-500"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{" "}
+              <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
+            </svg>
+            <h1>Market cap</h1>
+            <h3>{coin?.price}</h3>
+          </div>
+          <div className="flex items-center justify-around">
+            <svg
+              class="h-10 w-5 text-blue-500"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{" "}
+              <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
+            </svg>
+            <h1>Fully diluted market cap</h1>
+            <h3>{coin?.price}</h3>
+          </div>
+          <div className="flex items-center justify-around">
+            <svg
+              class="h-10 w-5 text-blue-500"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{" "}
+              <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
+            </svg>
+            <h1>All-time high (daily avg.)</h1>
+            <h3>{coin?.price}</h3>
+          </div>
+        </div>
+        <div>2</div>
+      </div>
     </div>
   );
 }
