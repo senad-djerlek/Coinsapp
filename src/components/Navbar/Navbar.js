03397930406279a80156a44bfaf17a1971c649cf
denismahmutovic <djdeny95@gmail.com>
2022-10-14 18:04:03 +0200

    size coins-page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,8 +12,8 @@ const NavBar = () => {
 
   return (
     <div className="sticky top-0 z-20 max-w-screen">
-      <nav className=" bg-blue-200 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 sticky bottom-0  ">
-        <div className="container flex flex-wrap justify-between items-center">
+      <nav className=" bg-blue-200 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 sticky bottom-0   ">
+        <div className="container flex flex-wrap justify-between items-center pl-[220px]">
           <div
             className="flex"
             style={{
--- src/pages/Coin/Coin.js
+++ src/pages/Coin/Coin.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import PaidOutlinedIcon from "@mui/icons-material/PaidOutlined";
 import CurrencyBitcoinOutlinedIcon from "@mui/icons-material/CurrencyBitcoinOutlined";
 import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
@@ -15,7 +16,7 @@ import TelegramIcon from "@mui/icons-material/Telegram";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 
 import SearchIcon from "@mui/icons-material/Search";
-import { Sparklines, SparklinesLine } from "react-sparklines";
+import { Sparklines, SparklinesLine, SparklinesSpots } from "react-sparklines";
 
 function Coin() {
   const [coin, setCoin] = useState();
@@ -34,6 +35,7 @@ function Coin() {
   };
   const getData = () => {
     axios.request(options).then(function (response) {
+      console.log(response.data.data.coin);
       setCoin(response.data.data.coin);
     });
   };
@@ -43,33 +45,32 @@ function Coin() {
   }, []);
 
   return (
-    <div className="mt-3">
+    <div className="ml-11 mt-5 mr-20 ">
       <div className="flex items-center justify-around w-2/5 mt-4 mb-5">
         <div className="flex   items-center ">
           <p className="flex justify-start">{coin?.rank}</p>
           <img className=" ml-3" src={coin?.iconUrl} alt="icon" width={38} />
           <p className="ml-3 font-bold">{coin?.name}</p>
 
-          <p className="ml-3">{coin?.symbol}</p>
-          <p className="ml-3 font-bold">
+          <p className="ml-3"> {coin?.symbol}</p>
+          <p className="ml-3 font-bold  ">
             ${Number(coin?.price).toLocaleString()}
           </p>
         </div>
 
-
-        <div className="text-blue-500">
-          <div className="absolute right-16 ">
-            <NavLink to="/" className="m-4">
+        <div className="text-blue-500 ">
+          <div className=" absolute right-16 ">
+            <NavLink to={"/"} className="m-4">
               Home
             </NavLink>
-            <NavLink className="m-4" to="/exchanges">
+            <NavLink className="m-4" to={"/exchanges"}>
               Exchanges
             </NavLink>
             <SearchIcon />
           </div>
         </div>
       </div>
-      <hr className="border-t-2 border-indigo-300" />
+      <hr className="border-t-2 border-indigo-200" />
 
       <div className="flex items-center justify-around mt-10  ">
         <h3 className="font-bold mb-2 ">Price chart </h3>
@@ -77,13 +78,11 @@ function Coin() {
           24h <span className="text-green-500"> {coin?.change} %</span>
         </p>
         <p>
-          High{" "}
-          <span className="font-bold">
-            ${Number(coin?.supply?.max).toLocaleString()}
-          </span>
+          {" "}
+          High <span className="font-bold">${coin?.supply?.max}</span>{" "}
         </p>
       </div>
-      <hr className="border-t-2 border-indigo-200" />
+      <hr className="border-t-2 border-indigo-200 " />
       <div className="mt-10 mb-12 max-w-screen">
         <Sparklines data={coin?.sparkline.map((el) => el)}>
           <SparklinesLine color="blue" />
@@ -104,19 +103,19 @@ function Coin() {
               <PaidOutlinedIcon fontSize="large" color="primary" />
             </div>
             <h1 className="w-28">Price to EUR</h1>
-            <h3 className="font-bold w-40">
-              {" "}
-              {Number(coin?.price).toLocaleString()}
-            </h3>
+            <h3 className="font-bold w-40"> {coin?.price}</h3>
           </div>
-          <hr className="border-t-2 border-indigo-300" />
+          <hr
+            className="border-t-2 border-indigo-200
+          "
+          />
           <div className="flex items-center justify-around mt-9">
             <div className="w-20 flex justify-center items-center">
               <CurrencyBitcoinOutlinedIcon fontSize="large" color="primary" />
             </div>
             <h1 className="w-28">Price to BTC</h1>
             <h3 className="font-bold w-40">
-              $ {Number(coin?.priceAt).toLocaleString()} {coin?.symbol}
+              $ {coin?.priceAt} {coin?.symbol}
             </h3>
           </div>
           <hr className="border-t-2 border-indigo-300" />
@@ -135,6 +134,7 @@ function Coin() {
             </div>
             <h1 className="w-28">24h volume</h1>
             <h3 className="font-bold w-40">
+              {" "}
               ${Number(coin?.price).toLocaleString()}
             </h3>
           </div>
@@ -144,9 +144,7 @@ function Coin() {
               <WaterOutlinedIcon fontSize="large" color="primary" />
             </div>
             <h1 className="w-28">Market cap</h1>
-            <h3 className="font-bold w-40">
-              ${Number(coin?.supply.supplyAt).toLocaleString()}
-            </h3>
+            <h3 className="font-bold w-40">${coin?.supply.supplyAt}</h3>
           </div>
           <hr className="border-t-2 border-indigo-300" />
           <div className="flex items-center justify-around mt-9">
@@ -154,17 +152,16 @@ function Coin() {
               <WaterOutlinedIcon fontSize="large" color="primary" />
             </div>
             <h1 className="w-28 ">Fully diluted market cap</h1>
-            <h3 className="font-bold w-40 ">
-              ${Number(coin?.supply.circulating).toLocaleString()}
-            </h3>
+            <h3 className="font-bold w-40 ">${coin?.supply.circulating}</h3>
           </div>
           <hr className="border-t-2 border-indigo-300" />
           <div className="flex items-center justify-around mt-9">
             <div className="w-20 flex justify-center items-center ">
               <BeenhereOutlinedIcon fontSize="large" color="primary" />
             </div>
+            <hr />
             <h1 className="w-28 ">All-time high (daily avg.)</h1>
-            <h3 className="w-40 font-bold">
+            <h3 className="w-40 mr-3">
               ${Number(coin?.supply.total).toLocaleString()}
             </h3>
           </div>
@@ -197,88 +194,92 @@ function Coin() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
               >
-                <path stroke="none" d="M0 0h24v24H0z" />
+                {" "}
+                <path stroke="none" d="M0 0h24v24H0z" />{" "}
                 <circle cx="12" cy="12" r="9" /> <path d="M9 12l2 2l4 -4" />
               </svg>
-              <h2 className="text-green-500 mt-9">Verified supplys</h2>
+              <h2 className="text-green-500 mt-9    ">Verified supplys</h2>
             </div>
             <div className="ml-3 mt-4  ">
               <p>Updated 2 minutes ago</p>
               <hr />
               <p className="mt-8 flex justify-between">
-                Total supply
+                Total supply{" "}
                 <span className="mr-5 font-bold">
-                  $ {Number(coin?.supply.circulating).toLocaleString()}
+                  {" "}
+                  $ {coin?.supply.circulating}
                 </span>
               </p>
               <hr />
               <p className="mt-11 flex justify-between">
-                Max supply
-                <span className=" mr-3 font-bold">
-                  $ {Number(coin?.supply.max).toLocaleString()}
-                </span>
+                Max supply{" "}
+                <span className=" mr-3 font-bold">$ {coin?.supply.max}</span>{" "}
               </p>
               <hr />
               <p className="mt-11 flex justify-between">
                 Total supply
                 <span className=" mr-4 font-bold">
-                  $ {Number(coin?.supply.total).toLocaleString()}
-                </span>
+                  $ {coin?.supply.total}
+                </span>{" "}
               </p>
             </div>
           </div>
         </div>
       </div>
-      <div className="max-w-screen flex">
+      <div className="max-w-screen flex  ">
         <div className="w-5/12">
           <h1 className="mt-9 text-center font-bold">What is {coin?.name}</h1>
+
           <div
-            className="px-3 pt-3 border border-indigo-600 rounded text-sm ml-3 mb-10 font-serif"
+            // style={{
+            //   color: `${coin?.color}`,
+            // }}
+            className="px-3 pt-3 border border-indigo-600 rounded text-sm ml-3 mb-10 font-serif m"
             dangerouslySetInnerHTML={{ __html: coin?.description }}
           />
         </div>
         <div className="w-1/2 ml-10 mt-14">
           <h1 className="ml-3 font-bold">Links</h1>
-          <div>
+          <div className="">
             <div className="flex  justify-between w-41">
               <div className="flex items-center w-32 justify-evenly font-bold">
                 <div className="w-10">
                   <LinkIcon fontSize="large" color="primary" />
                 </div>
-                <p>{link?.[0]}</p>
+                <p className="">{link?.slice(0, 1)}</p>
               </div>
-              <p>{name?.[0]}</p>
+              <p className="">{name?.slice(0, 1)}</p>
             </div>
             <hr className="border-t-2 border-indigo-100" />
             <div className="flex justify-between items-center mt-4 w-41 ">
-              <div className="flex items-center w-32 justify-evenly font-bold">
+              <div className="flex items-center  w-32 justify-evenly font-bold">
                 <div className="w-10 ">
                   <LinkIcon fontSize="large" color="primary" />
                 </div>
-                <p>{link?.[1]}</p>
+                <p>{link?.slice(1, 2)}</p>
               </div>
-              <p>{name?.[1]}</p>
+              <p>{name?.slice(1, 2)}</p>
             </div>
             <hr className="border-t-2 border-indigo-100" />
             <div className="flex justify-between items-center mt-4 w-41 ">
-              <div className="flex items-center w-32 justify-evenly font-bold">
+              <div className="flex items-center  w-32 justify-evenly font-bold">
                 <div className="w-10 ml-2">
                   <CurrencyBitcoinOutlinedIcon
                     fontSize="large"
                     color="primary"
                   />
                 </div>
-                <p>{link?.[2]}</p>
+                <p>{link?.slice(2, 3)}</p>
               </div>
-              <p>{name?.[2]}</p>
+              <p>{name?.slice(2, 3)}</p>
             </div>
             <hr className="border-t-2 border-indigo-100" />
             <div className="flex justify-between items-center mt-4 w-41 ">
               <div className="flex items-center  w-32 justify-evenly font-bold">
                 <CurrencyExchangeIcon fontSize="large" color="primary" />
-                <p>{link?.[3]}</p>
+                <p>{link?.slice(3, 4)}</p>
               </div>
-              <p>{name?.[3]}</p>
+              <p>{name?.slice(3, 4)}</p>
             </div>
             <hr className="border-t-2 border-indigo-100" />
             <div className="flex justify-between items-center mt-4 w-41 ">
@@ -286,9 +287,9 @@ function Coin() {
                 <div className="w-10 -ml-2">
                   <GitHubIcon fontSize="large" color="primary" />
                 </div>
-                <p>{link?.[4]}</p>
+                <p>{link?.slice(4, 5)}</p>
               </div>
-              <p>{name?.[4]}</p>
+              <p>{name?.slice(4, 5)}</p>
             </div>
             <hr className="border-t-2 border-indigo-100" />
 
@@ -296,20 +297,20 @@ function Coin() {
               <div className="flex items-center  w-32 justify-evenly font-bold">
                 <div className="w-10 -ml-2">
                   <RedditIcon fontSize="large" color="primary" />
-                </div>
-                <p>{link?.[5]}</p>
+                </div>{" "}
+                <p>{link?.slice(5, 6)}</p>
               </div>
-              <p>{name?.[5]}</p>
+              <p>{name?.slice(5, 6)}</p>
             </div>
             <hr className="border-t-2 border-indigo-100" />
             <div className="flex justify-between items-center mt-4 w-41 ">
               <div className="flex items-center  w-32 justify-evenly font-bold">
                 <div className="w-10 -ml-2">
                   <TelegramIcon fontSize="large" color="primary" />
-                </div>
-                <p>{link?.[6]}</p>
+                </div>{" "}
+                <p>{link?.slice(6, 7)}</p>
               </div>
-              <p>{name?.[6]}</p>
+              <p>{name?.slice(6, 7)}</p>
             </div>
             <hr className="border-t-2 border-indigo-200" />
             <div className="flex justify-between items-center mt-4 w-41 ">
@@ -317,9 +318,9 @@ function Coin() {
                 <div className="w-10">
                   <EventNoteIcon fontSize="large" color="primary" />
                 </div>
-                <p>{link?.[7]}</p>
+                <p>{link?.slice(8, 9)}</p>
               </div>
-              <p>{name?.[7]}</p>
+              <p>{name?.slice(8, 9)}</p>
             </div>
             <hr className="border-t-2 border-indigo-100" />
           </div>
